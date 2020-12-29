23b7a1b0e30fc113f20e9da1ab6d714d998dea28
Adam Nowland <acnowland@gmail.com>
2020-12-29 18:37:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   split_string = string.split(' ')
   puts split_string
   split_string.collect! do |word|
-    word.reverse.join(' ')
+    word.reverse
+    split_string.join(' ')
   end
 end
